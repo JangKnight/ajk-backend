2220560862f74e1195905407dc1b6f40db9dd3cf
Professor Knight <profdevent@gmail.com>
2026-03-23 01:51:56 -0400

    favicon fix

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from fastapi.middleware.trustedhost import TrustedHostMiddleware
 from fastapi.openapi.docs import get_swagger_ui_html
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
-favicon_path = os.path.join(base_dir, "favicon.ico")
+favicon_path = os.path.join(base_dir, "static", "favicon.ico")
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -54,7 +54,7 @@ async def custom_swagger_ui_html():
     return get_swagger_ui_html(
         title=app.title,
         openapi_url=app.openapi_url,
-        swagger_favicon_url="./favicon.ico"
+        swagger_favicon_url="/favicon.ico"
     )
 
 @app.get("/favicon.ico", include_in_schema=False)
